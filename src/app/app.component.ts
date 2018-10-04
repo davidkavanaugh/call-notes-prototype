417f0a3e0e616b6b3fc4893934ad027152898a03
David Kavanaugh <dkavanaugh@psgastro.com>
2018-10-04 15:54:32 -0700

    initial UI setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Response {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'callNotes';
+  selectedValue: string;
+
+  question1: Response[] = [
+    {value: 'Policy holder called to', viewValue: 'Yes'},
+    {value: 'Policy holder\'s spouse called to', viewValue: 'No, I\'m a spouse.'}
+  ];
+
+  question2: Response[] = [
+    {value: 'get a quote for', viewValue: 'Receive a quote'},
+    {value: 'file new claim', viewValue: 'File a claim'}
+  ];
+  question3: Response[] = [
+    {value: 'Question 3 Answer', viewValue: 'Answer'}
+  ];
+
+  question4: Response[] = [
+    {value: 'Question 4 Answer', viewValue: 'Answer'}
+  ];
+  question5: Response[] = [
+    {value: 'Question 5 Answer', viewValue: 'Answer'}
+  ];
+
+  question6: Response[] = [
+    {value: 'Question 6 Answer', viewValue: 'Answer'}
+  ];
+  question7: Response[] = [
+    {value: 'Question 7 Answer', viewValue: 'Answer'}
+  ];
+
+  question8: Response[] = [
+    {value: 'Question 8 Answer', viewValue: 'Answer'}
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
 import { AppComponent } from './app.component';
+import { MatInputModule, MatFormFieldModule, MatSelectModule, MatOptionModule } from '@angular/material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatOptionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
