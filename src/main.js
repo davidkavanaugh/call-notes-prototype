8f0fd7ab901ec32c69ac451e9200194699976451
David Kavanaugh <dkavanaugh@psgastro.com>
2018-10-08 16:50:19 -0700

    copy to clipboard and reset forms

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-new ClipboardJS('.copybtn');
-
 function copyToClipboard(){
-  let maintextbox = document.getElementById("main-textbox").textContent;
-  let additionalcomments = document.getElementById("additional-comments").textContent;
+  array = [document.querySelector("#notes").innerText]
+  if (document.querySelector("#additional-comments").innerText.length > 0){
+    array.push(document.querySelector("#additional-comments").innerText);
+  }
+let toCopy = array.join(" ");
 
-  let tocopy = maintextbox+'\n'+additionalcomments;
+let textarea = document.createElement("TEXTAREA");
+document.body.appendChild(textarea);
+var t = document.createTextNode(toCopy);
+textarea.appendChild(t);
 
-  alert(tocopy);
-  };
+document.querySelector('TEXTAREA').select();
+
+document.execCommand("copy");
+
+textarea.remove();
+}
+
+function reset(){
+  document.getElementById("form").reset();
+  document.getElementById("additional-comments").innerHTML="";
+}
+
